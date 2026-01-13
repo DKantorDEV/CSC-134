a89262d418d87d788bc9e769c2e481259af139da
DKantorDEV <d.kantor.dev@gmail.com>
2026-01-13 08:53:24 -0800

    Completed M1LAB.

--- M1LAB_Kantor.cpp
+++ M1LAB_Kantor.cpp
@@ -12,14 +12,20 @@ int main() {
     // Number of Apples Owned
     int apples = 100;
     // Price Per Apple
-   double pricePerApple = 0.25;
-    // Calculate the total price of the Apples
-    // TODO
+    double pricePerApple = 0.25;
 
     // Print all the information about the orchard
-    cout << "Welcome to " << name;
-    
+    // cout << "Welcome to " << name;
 
     cout << "Welcome to " << name;
     cout << "'s Apple Orchard." << endl;
+    cout << "We have " << apples;
+    cout << " apples in stock." << endl;
+    cout << "Apples are currently $";
+    cout << pricePerApple << " each." << endl;
+
+    // Calculate the total price of the Apples
+    double totalPrice = apples * pricePerApple;
+    cout << "To purchase all these apples, it will be $" << totalPrice;
+    cout << "." << endl;
 }
