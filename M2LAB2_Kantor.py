04f6c67cd062d4e3f28d3458521162ae7968369a
DKantorDEV <d.kantor.dev@gmail.com>
2026-02-08 17:05:17 -0500

    Added M2LAB2 with my initials

--- M2LAB2_Kantor.py
+++ M2LAB2_Kantor.py
@@ -1,65 +1,36 @@
-import turtle as t
-import random
+import turtle
+# This is the window / GUI
+window = turtle.Screen()
+window.setup(width=600, height=600)
+window.bgcolor("black")
+window.title("CSC-134 Lab - D.K.")
 
-t.colormode(255)
+# This is the turtle/pen
+bob = turtle.Turtle()
+bob.shape("turtle")
+bob.color("red")
+bob.pensize(5)
+bob.speed(1)
 
-# Make it faster!
-t.speed(0) # 0 = Fastest, 1 = Slowest, 6 = normal
+# The "D" for my initials DK
+bob.left(90)       
+bob.forward(100)     
+bob.right(90)         
+bob.circle(-50, 180)  
 
-# Make it prettier!
-t.bgcolor("black")
-t.pencolor("white")
+# This moves over to start the "K"
+bob.penup()          
+bob.goto(60, 0)      
+bob.setheading(90)   
+bob.pendown()         
 
-# Basic: Square
-for i in range(4):
-    t.forward(100)
-    t.right(90)
+# The "K" for my initials DK
+bob.forward(100)     
+bob.backward(50)     
+bob.right(45)      
+bob.forward(70)    
+bob.backward(70)     
+bob.right(90)         
+bob.forward(70)      
 
-# Evolving: Spiral
-for i in range(100):
-    t.forward(i * 2) # Gets bigger each time!
-    t.right(91) # Not quite 90.... see what happens?
-
-# Evolving: Rainbow Spiral
-for i in range(200):
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-
-    t.pencolor(r, g, b)
-
-    t.forward(i * 2)
-    t.right(91)
-
-# Turtle with boundaries
-for step in range(1000):
-    # Change color randomly as it moves
-    r, g, b = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
-    t.pencolor(r, g, b)
-    
-    t.forward(10)
-
-    # 1. Check Right Wall
-    if t.xcor() > 200:
-        t.setheading(180 - t.heading() + random.randint(-10, 10))
-        t.setx(199) # Gently push back so it doesn't get stuck
-
-    # 2. Check Left Wall
-    if t.xcor() < -200:
-        t.setheading(180 - t.heading() + random.randint(-10, 10))
-        t.setx(-199)
-
-    # 3. Check Top Wall
-    if t.ycor() > 200:
-        t.setheading(-t.heading() + random.randint(-10, 10))
-        t.sety(199)
-
-    # 4. Check Bottom Wall
-    if t.ycor() < -200:
-        t.setheading(-t.heading() + random.randint(-10, 10))
-        t.sety(-199)
-
-t.done()
-
-
-# Daniel, start over, this was just a learning practice. When the semester goes on return to this and start from scratch.
+window.exitonclick()
