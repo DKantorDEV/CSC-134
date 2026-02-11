9b52dfb554e776e2a218c519ec824db10b751a8a
DKantor2002 <d.kantor.dev@gmail.com>
2026-02-11 11:18:26 -0500

    Finished M3T2

--- M3T2_Kantor.cpp
+++ M3T2_Kantor.cpp
@@ -1,56 +1,74 @@
-// CSC 134
+// CSC-134
 // M3T2
 // Daniel Kantor
-// February 10, 2025
-// Random Dice rolls to see what we land on
+// February 11, 2025
+// Craps Game with C++
 
 
 #include <iostream>
-// pseudo-random numbers
 #include <cmath>    
-// making it truly random
 #include <ctime>
 using namespace std;
 
+// -------------------------------------------------- Declared our Variables/Rules---------------------------------------------------------------------
 
 int main() {
-    cout << "Let's roll some dice!" << endl;
+
+    // Declaring methods/variables for the game
+    cout << "Let's Play Craps" << endl;
     int seed = time(0);
     srand(seed);
-
     const int MAX = 6; // numbers from 1-6
     int roll1, roll2, total;
-    roll1  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
-    cout << "Your roll was: " << roll1 << endl;
 
-    roll2  = (rand() % MAX) + 1; // Divide by MAX, and just keep the remainder
-    cout << "Your roll was: " << roll2 << endl;
+// ---------------------------------------------------- First Roll of the Game ------------------------------------------------------------------------
 
+    // Initial Roll
+    roll1  = (rand() % MAX) + 1;
+    roll2  = (rand() % MAX) + 1;
     total = roll1 + roll2;
-    cout << "Your total roll is: " << total << endl;
+    cout << "You rolled: " << roll1 << " and " << roll2 << " for a total of " << total << endl;
 
-    // 7 and 11 - Automatic Win
-    // 2 and 12 - Automatic Lose
+// ---------------------------------------------------- Logic to the Win/Lose -------------------------------------------------------------------------
 
-    if (total == 7) {
-        cout << "You got 7! You Won!" << endl;
+    // Part 1: Initial Roll Logic
+    if (total == 7 || total == 11){
+        cout << "You got " << total << "! You won!" << endl;
     }
-    else if (total == 11) {
-        cout << "You got 11! You Won!" << endl;
-    }
-    else if (total == 2) {
-        cout << "Snake eyes! Too bad, you lose." << endl;
-    }
-    else if (total == 3) {
-        cout << "Sorry, three is unlucky, you lose." << endl;
-    }
-    else if (total == 12) {
-        cout << "Boxcars! Sorry, you lost." << endl;
+    else if (total == 2 || total == 3 || total == 12) {
+        cout << "You got " << total << "! You Lose!" << endl;
     }
     else {
-        // anything else
-        cout << "Your point is " << total << " but we'll do that part later" << endl;
+        // Part 2: Win System
+        int target = total; 
+        bool gameOver = false; // Bool to continue game or not
+        
+        cout << "Your target is now " << target << ". Roll Again!" << endl;
+
+        while (!gameOver) {
+            // Roll Again for Part 2
+            roll1 = (rand() % MAX) + 1;
+            roll2 = (rand() % MAX) + 1;
+            total = roll1 + roll2;
+
+            cout << "Next roll: " << roll1 << " + " << roll2 << " = " << total << endl;
+
+            // Check the new conditions
+            if (total == target) {
+                cout << "You hit your target! You Win!" << endl;
+                gameOver = true; // Player wins and game ends
+            } 
+            else if (total == 7) {
+                cout << "Seven out! You Lose!" << endl;
+                gameOver = true; // This breaks the loop
+            } 
+            else {
+                cout << "Roll Again!" << endl;
+            }
+        }
     }
 
     return 0;
 }
+
+// ---------------------------------------------------------------------------------------------------------------------------------------------------
